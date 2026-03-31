6c8ce00b8ba23fca828ad042a698a45809712bbe
Victor Vianna <victorvianna.hs@gmail.com>
2026-03-31 15:43:12 -0300

    metodotoString

--- src/Entidade/Produto.java
+++ src/Entidade/Produto.java
@@ -6,36 +6,47 @@ public class Produto {
     private double preco;
     private int quantidade;
 
-    public String getNome() {
-        return nome;
-    }
+
     public void setNome(String nome) {
+
         this.nome = nome;
     }
-    public double getPreco() {
-        return preco;
-    }
+
+
     public void setPreco(double preco) {
+
         this.preco = preco;
     }
-    public int getQuantidade() {
-        return quantidade;
-    }
+
+
     public void setQuantidade(int quantidade) {
+
         this.quantidade = quantidade;
     }
 
     public double valorTotal() {
+
         return preco * quantidade;
     }
 
     public void adicaoProduto(int quantidade) {
+
         this.quantidade += quantidade;
     }
 
     public void remocaoProduto(int quantidade) {
+
         this.quantidade -= quantidade;
     }
 
+    public String toString() {
+        return nome
+                + ", R$"
+                + String.format("%.2f", preco)
+                + ", "
+                + quantidade
+                + " unidades, Total: R$ "
+                + String.format("%.2f", valorTotal());
+    }
 
 }
--- src/Infos.java
+++ src/Infos.java
@@ -20,7 +20,9 @@ public class Infos {
         System.out.println("Quantidade em estoque: ");
         produto.setQuantidade(sc.nextInt());
 
-        System.out.printf("Informações do Produto: %s, R$ %.2f, %d unidades, Total: R$ %.2f%n ", produto.getNome(), produto.getPreco(), produto.getQuantidade(), produto.valorTotal());
+        System.out.println(produto);
+
+
 
         while (true) {
             System.out.println("Deseja atualizar a quantidade no estoque? (S/N):");
@@ -32,17 +34,18 @@ public class Infos {
                 if (decisao.equalsIgnoreCase("adicionar")) {
                     System.out.println("Digite a quantidade de produtos para adicionar ao estoque: ");
                     produto.adicaoProduto(sc.nextInt());
-                    System.out.printf("Atualização do produto: %s, R$ %.2f, %d unidades, Total: %.2f%n", produto.getNome(), produto.getPreco(), produto.getQuantidade(), produto.valorTotal());
+                    System.out.println("Atualização do produto: ");
+                    System.out.println(produto);
                 }else if(decisao.equalsIgnoreCase("remover")) {
                         System.out.println("Digite a quantidade de produtos para remover do estoque: ");
                         produto.remocaoProduto(sc.nextInt());
-                    System.out.printf("Atualização do produto: %s, R$ %.2f, %d unidades, Total: %.2f%n", produto.getNome(), produto.getPreco(), produto.getQuantidade(), produto.valorTotal());
+                    System.out.print("Atualização do Produto: ");
+                    System.out.println(produto);
                 }
             }else if (voto == 'n') {
                 break;
             }
         }
-
 
 
 
